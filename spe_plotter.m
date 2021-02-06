754736edfd39ce80c9f38dac2fbe6f651229107b
Srivatsa Chakravarthi <srivatsa@uw.edu>
2021-02-06 14:29:11 -0800

    Add files via upload
    Fixed some issues with plotting

--- spe_plotter.m
+++ spe_plotter.m
@@ -1,15 +1,15 @@
 %Change the path below to the required folder
-[files,dir] = uigetfile('D:\Dropbox\Projects\*.spe', 'MultiSelect','on'); 
+[files,dir] = uigetfile('D:\Dropbox\Projects\*1800*.spe', 'MultiSelect','on'); 
 if ischar(files)
     files = {files}; 
 end
 
 %-------------------------------------------------------------------------
 % %User options
-stiched_spectra = 0;  %is this a winspec stitched spectra (i.e pixels > ccd_width ?)
+stitched_spectra = 0;  %is this a winspec stitched spectra (i.e pixels > ccd_width ?)
 wl_calibrated = 1;    %is the winspec x-axis data calibrated ?
 new_spectrometer = 1; %is this the new black (HP) spectrometer or the old blue box
-try_fit = 0;          %try to fit the peak to a lorentzian (for cavity Q-analysis)
+try_fit = 1;          %try to fit the peak to a lorentzian (for cavity Q-analysis)
 bkg_subtract = 0;     %try a crude bkg subtration (mean of the first 100 pixels)
 integrate_curve = 0;  %integrate to find area under the curve?
 save_plot_png = 0;    %save a png file?
@@ -76,27 +76,29 @@ for n=1:length(files)
         set(h,'DisplayStyle','datatip');
         waitfor(gcf,'CurrentCharacter',char(32));
         s = getCursorInfo(h);
-        start_x=s.Position(1);
-
+        start_x = s.Position(1);
+        start_val = s.Position(2);
+        start_ind = find(data_x == start_x);
+        
         %Define a lorentzian fit
         lorentz = fittype('e1*x + d1 + a1*c1./(2*pi*( ((x-b1).^2) + ((c1/2)^2) ))', ...
                                 'dependent',{'y'},'independent',{'x'}, ...
                                 'coefficients',{'a1','b1','c1','d1','e1'});
          
         %Try the fit function                                                                                        
-        try
-            f1 = fit(data_x(m_ind-100:m_ind+100), data_y(m_ind-100:m_ind+100), ...
+       % try
+            f1 = fit(data_x(start_ind-100:start_ind+100), data_y(start_ind-100:start_ind+100), ...
                 lorentz, 'StartPoint',[100, start_x, 10*dx, 500,1], ...
-                'Upper', [m, start_x + 10*dx, 50*dx, 2*m,1000], ...
+                'Upper', [start_val, start_x + 10*dx, 50*dx, 2*start_val,1000], ...
                 'Lower', [1, start_x - 30*dx, 2*dx, 0,-1000]);
             
             %Update plot and print results
             hold on;
             plot(f1);
             title(['Fit of scan ' num2str(n) ' width=' num2str(f1.c1) ' Q=' num2str(f1.b1/f1.c1)]);
-            
+          
             fprintf('FWHM = %2.3f \t Q = %4.1f \n',f1.c1, f1.b1/f1.c1)        
-        end
+       % end
         
     else
         
